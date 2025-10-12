6b8674795acc80109fd54a8e871a2e47589bf697
ifuller140 <ianfuller140@gmail.com>
2025-10-12 03:04:54 -0400

    Fix youtube media prioritization

--- components/ProjectMedia.tsx
+++ components/ProjectMedia.tsx
@@ -14,13 +14,7 @@ export default function ProjectMedia({
   const isMp4 = media && media.includes('.mp4');
   const isYoutube = media && media.includes('www.youtube.com');
 
-  if (isMp4) {
-    return (
-      <video className="w-full bg-gray" width="854" height="480" controls>
-        <source src={`/${media}`} type="video/mp4" />
-      </video>
-    );
-  } else if (isYoutube) {
+  if (isYoutube) {
     return (
       <iframe
         className="w-full aspect-video"
@@ -28,6 +22,12 @@ export default function ProjectMedia({
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
       ></iframe>
+    );
+  } else if (isMp4) {
+    return (
+      <video className="w-full bg-gray" width="854" height="480" controls>
+        <source src={`/${media}`} type="video/mp4" />
+      </video>
     );
   } else {
     return (
